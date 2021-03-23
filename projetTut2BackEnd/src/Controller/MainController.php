a661feeeecc2132aa8a3df0d1e42c3a10162ee98
Selene VIOLA <selene.viola9@etu.univ-lorraine.fr>
2021-03-23 15:34:34 +0100

    Ajout point api Images

--- projetTut2BackEnd/src/Controller/MainController.php
+++ projetTut2BackEnd/src/Controller/MainController.php
@@ -6,7 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Elasticsearch\ClientBuilder;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 
 class MainController extends AbstractController
 {
@@ -18,10 +17,10 @@ class MainController extends AbstractController
     }
 
     /**
-     * @Route("/game/{id}", name="game")
-     * @param $id
+     * @Route("/game/{id}", name="game", methods={"GET"})
+     * @param string $id
      */
-    public function game($id)
+    public function game(string $id)
     {
         $params = [
             'index' => 'steam',
@@ -31,11 +30,11 @@ class MainController extends AbstractController
         $client = ClientBuilder::create()->setHosts(['localhost:9200'])->build();
 
         $result = $client->get($params);
-        dd($result);
+        //dd($result);
     }
 
     /**
-     * @Route("/games/{page}", name="games")
+     * @Route("/games/{page}", name="games", requirements={"page" = "\d+"}, methods={"GET"})
      * @param int $page
      * @return JsonResponse
      */
@@ -58,5 +57,27 @@ class MainController extends AbstractController
         return new JsonResponse($result);
 
         //dd($result);
+    }
+
+    /**
+     * @Route("/game/{appid}/images", name="images_game", methods={"GET"})
+     * @param string $appid
+     */
+    public function images(string $appid)
+    {
+        $params = [
+            'index' => 'steam_media_data',
+            'body' => [
+                'query' => [
+                    'match' => [
+                        'data.steam_appid' => $appid
+                    ]
+                ]
+            ]
+        ];
+
+        $client = ClientBuilder::create()->setHosts(['localhost:9200'])->build();
+        $results = $client->search($params);
+        return new JsonResponse($results);
     }
 }
