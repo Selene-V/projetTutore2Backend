cfb9992fafc28c18425aab5885b7964e4d4f8268
Selene VIOLA <selene.viola9@etu.univ-lorraine.fr>
2021-03-30 11:26:33 +0200

    Debug du script csv, résolution bug donnees manquantes dans elasticsearch

--- Script/script_csv.js
+++ Script/script_csv.js
@@ -3,51 +3,74 @@ const client = new Client({ node: "http://localhost:9200" });
 const fs = require("fs");
 
 const csv = require("fast-csv");
-
-let iterator = 0;
-
 function createBDD(file) {
-  let data = {};
-  const stream = fs
-    .createReadStream("CSV/" + file + ".csv")
-    .pipe(csv.parse({ headers: true }))
-    .on("data", function (dataFile) {
-      for (let [key, value] of Object.entries(dataFile)) {
-        if (!isNaN(value)){
-          if (parseInt(value) === parseFloat(value)){
-            value = parseInt(value);
-          } else {
-            value = parseFloat(value);
+  return new Promise((resolve) => {
+    console.log("Creating index", file);
+    let inserts = [];
+    const stream = fs
+        .createReadStream("CSV/" + file + ".csv")
+        .pipe(csv.parse({ headers: true }))
+        .on("data", function (dataFile) {
+          let data = {};
+          for (let [key, value] of Object.entries(dataFile)) {
+            if (key === "price") {
+              continue;
+            }
+            if (!isNaN(value)) {
+              value = parseInt(value);
+            }
+            data[key] = value;
           }
-        }
-        data[key] = value;
-      };
-      // const dataJson = JSON.stringify(dataOk);
-      // Let's start by indexing some data
-      client.index({
-        index: file,
-        body: {
-          data,
-        },
-      });
-    })
-    .on("end", function () {
-      iterator++;
-      console.log("Read finish");
-      if (iterator === 6) {
-        console.log("Script finished");
-        return process.exit(22);
-      }
-    });
+          // const dataJson = JSON.stringify(dataOk);
+          // Let's start by indexing some data
+          inserts.push(
+              client.index({
+                index: file,
+                body: {
+                  data,
+                },
+              })
+          );
+        })
+        .on("end", async function () {
+          console.log("Read finish", file);
+          console.log("Waiting for inserts to finish...");
+          await Promise.all(inserts);
+          console.log("Inserts finished", file);
+          resolve();
+        });
+  });
 }
 
-function init() {
-  createBDD("steam");
-  createBDD("steam_description_data");
-  createBDD("steam_media_data");
-  createBDD("steam_requirements_data");
-  createBDD("steam_support_info");
-  createBDD("steamspy_tag_data");
+const indexes = [
+  "steam",
+  "steam_description_data",
+  "steam_media_data",
+  "steam_requirements_data",
+  "steam_support_info",
+  "steamspy_tag_data",
+];
+
+async function purgeDB() {
+  console.log("Purging current indexes if they exist");
+  let promises = indexes.map((item) => client.indices.delete({ index: item }));
+  await Promise.allSettled(promises);
+  console.log("Purging finished");
+}
+
+async function createDB() {
+  console.log("Creating new indexes with current dataset");
+  for (let i = 0; i < indexes.length; i++) {
+    await createBDD(indexes[i]);
+  }
+  console.log("All indexes created");
+}
+
+async function init() {
+  await purgeDB();
+  await createDB();
+  console.log("Script finished successfully");
+  process.exit(0);
 }
 
 init();
