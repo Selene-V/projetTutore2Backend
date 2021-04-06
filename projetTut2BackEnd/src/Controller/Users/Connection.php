09ade1e6888875e5551cec8d6bdf8235e74d0645
Matthieu <matth.fra@gmail.com>
2021-04-06 12:16:13 +0200

    Connexion + déconnexion

---
+++ projetTut2BackEnd/src/Controller/Users/Connection.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller\Users;
+
+use Symfony\Component\Routing\Annotation\Route;
+use PDO;
+use Symfony\Component\HttpFoundation\Response;
+
+class Connection
+{
+    /**
+     * @Route("/connection", name="connection", methods={"POST"})
+     */
+    public function connection()
+    {
+        $bdd = new PDO('mysql:host=127.0.0.1;dbname=projettutore2', 'root', '');
+
+        $email = $_POST['email'];
+
+        $req = $bdd->prepare('SELECT id, password FROM user WHERE email = :email');
+        $req->execute(array(
+            'email' => $email
+        ));
+        $result = $req->fetch();
+
+        $isPasswordCorrect = password_verify($_POST['password'], $result['password']);
+
+        if (!$result) {
+            return new Response('Wrong login or password !');
+        } else {
+            if ($isPasswordCorrect) {
+                session_start();
+                $_SESSION['id'] = $result['id'];
+                $_SESSION['email'] = $email;
+                return new Response('You are connected !');
+            } else {
+                return new Response('Wrong login or password !');
+            }
+        }
+    }
+}
