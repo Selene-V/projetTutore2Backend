e2356d79858f79be3ec92297b60e5730d613afd1
Selene-V <71885295+Selene-V@users.noreply.github.com>
2021-04-08 15:30:31 +0200

    debug

--- projetTut2BackEnd/src/Controller/Users/Register.php
+++ projetTut2BackEnd/src/Controller/Users/Register.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Users;
 
+use App\Config\Config;
 use App\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use PDO;
@@ -16,7 +17,18 @@ class Register extends AbstractController
      */
     public function register(Request $request)
     {
-        $bdd = new PDO('mysql:host=127.0.0.1;dbname=projettutore2', 'root', '');
+        $dbname = Config::config('pdo_dbname');
+        $host = Config::config('pdo_host');
+        $user = Config::config('pdo_user');
+        $password = Config::config('pdo_password');
+
+        $dsn = sprintf(
+            'mysql:dbname=%s;host=%s',
+            $dbname,
+            $host
+        );
+
+        $bdd = new PDO($dsn, $user, $password);
 
         $requestContent = $request->getContent();
 
