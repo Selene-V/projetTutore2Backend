797190ec2a443bc14b7c59f6480ee868393d3c0a
Matthieu <matth.fra@gmail.com>
2021-04-09 11:34:27 +0200

    Résolution soucis overwrite git

--- projetTut2BackEnd/src/Controller/Users/UserController.php
+++ projetTut2BackEnd/src/Controller/Users/UserController.php
@@ -15,28 +15,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class UserController extends AbstractController
 {
     private PDO $bdd;
-
     /**
      * UserController constructor.
      */
     public function __construct()
     {
         parent::__construct();
-
         $dbname = Config::config('pdo_dbname');
         $host = Config::config('pdo_host');
         $user = Config::config('pdo_user');
         $password = Config::config('pdo_password');
-
         $dsn = sprintf(
             'mysql:dbname=%s;host=%s',
             $dbname,
             $host
         );
-
         $this->bdd = new PDO($dsn, $user, $password);
     }
-
     /**
      * @Route("/addGameToLibrary", name="addGameToLibrary", methods={"POST"})
      * @param Request $request
@@ -45,25 +40,17 @@ class UserController extends AbstractController
     public function addGameToLibrary(Request $request): Response
     {
         $requestContent = $request->getContent();
-
         $searchParams = $this->parseRequestContent($requestContent);
-
         $data = $this->getToken($request);
-
         if ($this->checkToken($data)) {
             $select = $this->bdd->prepare("SELECT * FROM users_games WHERE user = :user AND game = :game");
-
             $select->bindParam(':user', $data['id'], PDO::PARAM_INT);
             $select->bindParam(':game', $searchParams['game'], PDO::PARAM_INT);
-
             $select->execute();
-
             if ($select->rowCount() === 0) { //On vérifie si la relation n'existe pas déjà
                 $req = $this->bdd->prepare("INSERT INTO users_games VALUES (:user, :game)");
-
                 $req->bindParam(':user', $data['id'], PDO::PARAM_INT);
                 $req->bindParam(':game', $searchParams['game'], PDO::PARAM_INT);
-
                 if ($req->execute()) {
                     return new Response(true);
                 }
@@ -72,7 +59,6 @@ class UserController extends AbstractController
         }
         return new Response(false);
     }
-
     /**
      * @Route("/removeGameFromLibrary", name="removeGameFromLibrary", methods={"POST"})
      * @param Request $request
@@ -81,17 +67,12 @@ class UserController extends AbstractController
     public function removeGameFromLibrary(Request $request): Response
     {
         $requestContent = $request->getContent();
-
         $searchParams = $this->parseRequestContent($requestContent);
-
         $data = $this->getToken($request);
-
         if ($this->checkToken($data)) {
             $req = $this->bdd->prepare("DELETE FROM users_games WHERE user = :user AND game = :game");
-
             $req->bindParam(':user', $data['id'], PDO::PARAM_INT);
             $req->bindParam(':game', $searchParams['game'], PDO::PARAM_INT);
-
             if ($req->execute()) {
                 return new Response(true);
             }
@@ -103,24 +84,18 @@ class UserController extends AbstractController
     /**
      * @Route("/displayLibrary", name="display_library", requirements={"page" = "\d+"}, methods={"POST"})
      * @param Request $request
-     * @return JsonResponse
+     * @return Response
      */
-    public function displayLibrary(Request $request): JsonResponse
+    public function displayLibrary(Request $request): Response
     {
-
         $requestContent = $request->getContent();
-
         $searchParams = $this->parseRequestContent($requestContent);
-
         $data = $this->getToken($request);
-
         if ($this->checkToken($data)) {
             $req = $this->bdd->prepare('SELECT game FROM users_games WHERE user = :user');
-
             $req->execute(array(
                 'user' => $data['id']
             ));
-
             $resultSQL = $req->fetchAll();
 
             if (empty($resultSQL)) {
@@ -129,56 +104,39 @@ class UserController extends AbstractController
 
             $gamesByPage = 8;
             $page = $searchParams['page'];
+
             if ($page < 1) {
                 $page = 1;
             }
-
-            if ($page !== null) {
-                $params = [
-                    'index' => 'steam',
-                    'size' => $gamesByPage,
-                    'from' => ($page - 1) * $gamesByPage,
-                    'body' => [
-                        'query' => [
-                            'bool' => [
-                                'should' => [],
-                            ],
+            $params = [
+                'index' => 'steam',
+                'size' => $gamesByPage,
+                'from' => ($page - 1) * $gamesByPage,
+                'body' => [
+                    'query' => [
+                        'bool' => [
+                            'should' => [],
                         ],
                     ],
-                ];
-            } else {
-                $params = [
-                    'index' => 'steam',
-                    'body' => [
-                        'query' => [
-                            'bool' => [
-                                'should' => [],
-                            ],
-                        ],
-                    ],
-                ];
-            }
+                ],
+            ];
 
 
             $queryParams = [];
-
             foreach ($resultSQL as $game) {
                 array_push($queryParams, array("terms" => array('data.appid' =>  (array)$game[0])));
             }
-
             $params['body']['query']['bool']['should'] = $queryParams;
-
             $result = $this->client->search($params);
-
             $games = ['games' => []];
             foreach ($result['hits']['hits'] as $gameInfos) {
                 $idgame = $gameInfos['_source']['data']['appid'];
-
                 $image = $this->createImage($idgame);
-
+                $description = $this->createDescription($idgame);
                 $game = new Game();
                 $game->hydrate($gameInfos['_source']['data']);
                 $game->setImage($image);
+                $game->setDescription($description);
                 $game->setId($gameInfos['_id']);
                 array_push($games['games'], json_decode($this->serializer->serialize($game, 'json')));
             }
@@ -188,25 +146,60 @@ class UserController extends AbstractController
             unset($params['from']);
 
             $totalGames = $this->client->count($params);
-
             $games['nbPages'] = ceil($totalGames['count'] / $gamesByPage);
-
             return new JsonResponse($games);
         }
         return new Response(false);
     }
 
-    public function getToken(Request $request)
+    /**
+     * @Route("/libraryContains", name="library_contains", methods={"POST"})
+     * @param Request $request
+     * @return Response
+     */
+    public function libraryContains(Request $request): Response
+    {
+
+        $requestContent = $request->getContent();
+
+        $searchParams = $this->parseRequestContent($requestContent);
+
+        $data = $this->getToken($request);
+
+        if ($this->checkToken($data)) {
+            $req = $this->bdd->prepare('SELECT game FROM users_games WHERE user = :user AND game = :game');
+
+            $req->execute(array(
+                'user' => $data['id'],
+                'game' => $searchParams['appid']
+            ));
+
+            $resultSQL = $req->fetch();
+        }
+        if (!$resultSQL) {
+            return new Response($resultSQL);
+        } else {
+            return new Response(true);
+        }
+    }
+
+    /**
+     * @param Request $request
+     * @return array
+     */
+    public function getToken(Request $request): array
     {
         $authorizationHeader = $request->headers->get('Authorization');
         $authorizationHeaderArray = explode(' ', $authorizationHeader);
         $token = $authorizationHeaderArray[0] ?? null;
         $data = (new TokenManager())->decode($token);
-
         return $data;
     }
-
-    public function checkToken($data)
+    /**
+     * @param $data
+     * @return bool
+     */
+    public function checkToken($data): bool
     {
         if ($data['exp'] > time()) {
             return true;
